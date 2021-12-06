046eded8ff22369d213ad36598673dfdc4a483c6
Alex Sartori <alex.sartori1997@gmail.com>
2021-12-06 12:52:10 +0100

    Improve Day 6 pt. 2

--- Day 06/2.rb
+++ Day 06/2.rb
@@ -1,17 +1,13 @@
 fish = Hash.new(0)
-gets.split(',').map(&:to_i).each do |v|
-  fish[v] += 1
-end
+gets.split(',').map(&:to_i).each { |v| fish[v] += 1 }
 
 (0..255).each do |day|
   fish[9] = fish[0]
   fish[7] += fish[0]
   fish[0] = 0
-  
-  (0..8).each do |i|
-    fish[i] = fish[i+1]
-  end
 
+  (0..8).each { |i| fish[i] = fish[i + 1] }
+  
   fish[9] = 0
 end
 
