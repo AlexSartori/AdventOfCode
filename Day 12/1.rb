f22c0b65ad860be5567a7a634657a4ed7d7a151e
Alex Sartori <alex.sartori1997@gmail.com>
2022-12-16 15:08:51 +0100

    Add Day 12

---
+++ Day 12/1.rb
@@ -0,0 +1,40 @@
+$grid = STDIN.read.split("\n").map(&:chars)
+$steps = []
+$s, $e = nil, nil
+$h, $w = $grid.size, $grid[0].size
+
+$grid.each_with_index do |row, y|
+    $steps << [-1] * row.size
+    _s, _e = row.index('S'), row.index('E')
+    row.map! &:ord
+    $s = [_s, y] unless _s.nil?
+    $e = [_e, y] unless _e.nil?
+end
+
+def should_move from, to
+    fy, fx, ty, tx = *from, *to
+    return false if fy < 0 or fy >= $h or fx < 0 or fx >= $w
+    return false if ty < 0 or ty >= $h or tx < 0 or tx >= $w
+    return false if $grid[ty][tx] > $grid[fy][fx] + 1
+    return true
+end
+
+def recursive_walk y, x
+    n = $steps[y][x]
+    
+    for y_off, x_off in [[-1, 0], [0, 1], [1, 0], [0, -1]] do 
+        if should_move([y, x], [y+y_off, x+x_off]) and ($steps[y+y_off][x+x_off] == -1 or $steps[y+y_off][x+x_off] > n + 1)
+            $steps[y+y_off][x+x_off] = n + 1
+            recursive_walk y+y_off, x+x_off
+        end
+    end
+end
+
+
+$steps[$s[1]][$s[0]] = 0
+$grid[$s[1]][$s[0]] = 'a'.ord
+$grid[$e[1]][$e[0]] = 'z'.ord
+
+recursive_walk $s[1], $s[0]
+
+p $steps[$e[1]][$e[0]]
--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Previous year: [2021 branch](https://github.com/AlexSartori/AdventOfCode/tree/20
 - [Day 09: Rope Bridge](https://adventofcode.com/2022/day/9)
 - [Day 10: Cathode-Ray Tube](https://adventofcode.com/2022/day/10)
 - [Day 11: Monkey in the Middle](https://adventofcode.com/2022/day/11)
+- [Day 12: Hill Climbing Algorithm](https://adventofcode.com/2022/day/12)
